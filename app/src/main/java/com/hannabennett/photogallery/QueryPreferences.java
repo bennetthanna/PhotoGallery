ea9d795ed5ccfc5c6053db7d70be94dfafa35a5b
Hanna Bennett <bennetthanna22@gmail.com>
2017-12-13 22:15:50 -0800

    Chapter 27, page 532 : Add class to serve as a convenient interface for reading and writing the query to and from shared preferences

---
+++ app/src/main/java/com/hannabennett/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package com.hannabennett.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by HannaBennett on 12/13/17.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
