f3748ac213d9a03a431bf57b6eaccbae76419384
Hanna Bennett <bennetthanna22@gmail.com>
2017-12-24 18:20:18 -0800

    Chapter 28, page 544 : Add alarm method to send intents for you

--- app/src/main/java/com/hannabennett/photogallery/PollService.java
+++ app/src/main/java/com/hannabennett/photogallery/PollService.java
@@ -1,12 +1,16 @@
 package com.hannabennett.photogallery;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by HannaBennett on 12/21/17.
@@ -15,8 +19,26 @@ import java.util.List;
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
+    // Set interval to 1 minute
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn) {
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime(), POLL_INTERVAL_MS, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
     }
 
     public PollService() {
